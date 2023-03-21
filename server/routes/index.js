89395b78e39f4277ff5232ea1306d4adf14f032e
Wyn Chrsitian <wynchristian.rebanal@tup.edu.ph>
2023-03-21 22:28:42 +0800

    Update index router in index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  return res.json({
+    message: "This is the Bakery System's index router",
+  });
 });
 
 module.exports = router;
