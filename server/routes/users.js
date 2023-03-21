ef3b03ec788cd2d4784cb68985511fc15f7b84e9
Wyn Chrsitian <wynchristian.rebanal@tup.edu.ph>
2023-03-21 22:34:17 +0800

    Added update function in users

--- server/routes/users.js
+++ server/routes/users.js
@@ -6,6 +6,7 @@ const user_controller = require("../controllers/user");
 /* GET users listing. */
 router.post("/login", user_controller.login);
 router.post("/create", user_controller.create);
+router.post("/:id/update", user_controller.update);
 router.post("/check-user", user_controller.check_user);
 router.get("/:id", user_controller.detail);
 router.get("/", user_controller.list);
